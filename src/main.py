976ab8d2efe7447a743fa6a4e06b36c98893d5d7
Dylan <rattledew17@gmail.com>
2024-09-05 18:11:53 -0700

    added new way to save topics

--- src/main.py
+++ src/main.py
@@ -18,40 +18,27 @@ class App:
         topic_window.geometry("480x440")
         topic_window.config(bg=BACKGROUND_COLOR)
         
-        topic_instruction = Util.create_label(topic_window)
-        topic_instruction.config(text="<Ctrl + S> to close window and save.",
-                                 pady=10)
-        topic_instruction.pack(side=TOP)
-        
         topic_entry = Util.create_text(topic_window)
-        # restores any previous saved data
-        with open("../data/topic.json", "r") as file:
-            topic_example = json.load(file)
-            
-        topic_entry.insert(END, "".join([word for word in topic_example[request]]))
-        topic_entry.bind("<Control-s>", lambda e: App.add_topic(request,
-                                                                topic_entry,
-                                                                topic_window))
         topic_entry.pack()
         
         add_topic_button = Util.create_button(topic_window)
         add_topic_button.config(image=add_topic_image,
-                                command=lambda: print("added"))
+                                command=lambda: App.add_topic(request,
+                                                              topic_entry))
         add_topic_button.pack()
     
     # adds the inserted topic into topic.json
     def add_topic(request: str,
-                  topic_entry,
-                  topic_window):
+                  topic_entry):
         with open("../data/topic.json", "r") as file:
             topic_example = json.load(file)
         
-        topic_example[request] = topic_entry.get("1.0", END)
+        topic_example[request].append(topic_entry.get("1.0", END))
         
         with open("../data/topic.json", "w") as file:
             json.dump(topic_example, file, indent=4, sort_keys=True)
         
-        topic_window.destroy()
+        topic_entry.delete("0.0", END)
 
 topic_button = Util.create_button(root)
 topic_button.config(text="add topic",
