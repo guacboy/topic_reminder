f101ed7d24bc5884eb30ff5bacf3b4480eff3173
Dylan <rattledew17@gmail.com>
2024-09-07 16:59:57 -0700

    added functionality to updating topic.json and deleting topics

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ root.config(bg=BACKGROUND_COLOR)
 
 add_topic_image = ImageTk.PhotoImage(Image.open("assets/start-button.png"))
 
-current_topic_display = []
+current_reminder_list = []
     
 class App:
     # type your topics
-    def create_topic_window(request: str):
+    def create_topic_window():
         topic_window = Toplevel()
         topic_window.geometry("480x440")
         topic_window.config(bg=BACKGROUND_COLOR)
@@ -26,18 +26,16 @@ class App:
         
         add_topic_button = Util.create_button(topic_window)
         add_topic_button.config(image=add_topic_image,
-                                command=lambda: App.add_topic(request,
-                                                              topic_entry))
+                                command=lambda: App.add_topic(topic_entry))
         add_topic_button.pack()
     
     # adds the inserted topic into topic.json
-    def add_topic(request: str,
-                  topic_entry):
+    def add_topic(topic_entry):
         with open("../data/topic.json", "r") as file:
             topic_list = json.load(file)
-        
+            
         # gets the topic entered and saves it into topic.json
-        topic_list[request].append(topic_entry.get("1.0", END))
+        topic_list["topic"].append(topic_entry.get("1.0", END).strip())
         
         with open("../data/topic.json", "w") as file:
             json.dump(topic_list, file, indent=4, sort_keys=True)
@@ -50,6 +48,8 @@ class App:
         reminder_window.geometry("480x840")
         reminder_window.config(bg=BACKGROUND_COLOR)
         
+        # ensures the current topics displayed is reset
+        current_reminder_list.clear()
         App.display_reminder(reminder_window)
         
     def display_reminder(window):
@@ -59,53 +59,68 @@ class App:
             
             # chooses a random reminder from topic.json
             while True:
-                random_reminder = random.choice(topic_list["topic"])
+                reminder = random.choice(topic_list["topic"])
                 
                 # avoids displaying repeated topics
-                if random_reminder not in current_topic_display:
-                    current_topic_display.append(random_reminder)
+                if reminder not in current_reminder_list:
+                    current_reminder_list.append(reminder)
                     break
-            
-            # creates a random tag (variable)
-            random_reminder_tag = "".join([random.choice("1234567890") for tag in range(6)])
             
             reminder_frame = Util.create_frame(window)
             reminder_frame.pack()
             
-            random_reminder_tag = Util.create_label(reminder_frame)
-            random_reminder_tag.config(text=random_reminder)
-            random_reminder_tag.pack()
+            reminder_label = Util.create_label(reminder_frame)
+            reminder_label.config(text=reminder)
+            reminder_label.pack()
             
-            # hovering makes the approve/decline buttons appear
-            reminder_frame.bind("<Enter>", lambda e: App.toggle_reminder_option_button(e, reminder_frame, True))
+            # hovering makes the done/skip buttons appear
+            reminder_frame.bind("<Enter>", lambda e: App.toggle_reminder_option_button(e, reminder_frame, reminder_label, True))
             # otherwise, makes them disappear
-            reminder_frame.bind("<Leave>", lambda e: App.toggle_reminder_option_button(e, reminder_frame, False))
+            reminder_frame.bind("<Leave>", lambda e: App.toggle_reminder_option_button(e, reminder_frame, reminder_label, False))
             
-            if len(current_topic_display) < len(topic_list["topic"]):
-                root.after(3000, App.display_reminder, window)
+            if len(current_reminder_list) < len(topic_list["topic"]):
+                root.after(2000, App.display_reminder, window)
     
-    # toggles approve/decline buttons below a reminder
+    # toggles done/skip buttons below a reminder
     def toggle_reminder_option_button(e,
-                                      frame,
-                                      active: bool):
-        if not hasattr(frame, "approve_button"):
-            frame.approve_button = Util.create_button(frame)
-            frame.approve_button.config(image=add_topic_image, command=lambda: print("approve"))
+                                      reminder_frame: str,
+                                      reminder_label,
+                                      is_active: bool):
+        if not hasattr(reminder_frame, "skip_button"):
+            reminder_frame.skip_button = Util.create_button(reminder_frame)
+            reminder_frame.skip_button.config(image=add_topic_image,
+                                              command=lambda: modify_json_file(reminder_label, False))
         
-        if not hasattr(frame, "decline_button"):
-            frame.decline_button = Util.create_button(frame)
-            frame.decline_button.config(image=add_topic_image, command=lambda: print("decline"))
+        if not hasattr(reminder_frame, "done_button"):
+            reminder_frame.done_button = Util.create_button(reminder_frame)
+            reminder_frame.done_button.config(image=add_topic_image,
+                                              command=lambda: modify_json_file(reminder_label, True))
 
-        if active:
-            frame.approve_button.pack(side=LEFT)
-            frame.decline_button.pack()
+        # checks if cursor is hovering over frame
+        if is_active:
+            reminder_frame.skip_button.pack(side=LEFT)
+            reminder_frame.done_button.pack()
         else:
-            frame.approve_button.pack_forget()
-            frame.decline_button.pack_forget()
+            reminder_frame.skip_button.pack_forget()
+            reminder_frame.done_button.pack_forget()
+        
+        def modify_json_file(reminder_label,
+                             is_delete: bool):
+            reminder_frame.pack_forget()
+            
+            if is_delete:
+                with open("../data/topic.json", "r") as file:
+                        topic_list = json.load(file)
+                
+                # finds the matching text and deletes it from topic.json
+                topic_list["topic"].remove(reminder_label.cget("text"))
+                    
+                with open("../data/topic.json", "w") as file:
+                    json.dump(topic_list, file, indent=4, sort_keys=True)
 
 topic_button = Util.create_button(root)
 topic_button.config(image=add_topic_image,
-                    command=lambda: App.create_topic_window("topic"))
+                    command=lambda: App.create_topic_window())
 topic_button.place(anchor=CENTER,
                    relx=0.5,
                    rely=0.5)
