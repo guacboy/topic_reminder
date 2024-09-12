6e19ad4c59a64c3705ea00802afe7c989dc67f92
Dylan <rattledew17@gmail.com>
2024-09-12 14:10:58 -0700

    added details to widgets

--- src/util.py
+++ src/util.py
@@ -6,16 +6,30 @@ FONT_TYPE = "Roobert"
 
 class Util:
     def create_button(window):
-        return Button(window)
+        return Button(window,
+                      bg=BACKGROUND_COLOR,
+                      activebackground=BACKGROUND_COLOR,
+                      bd=0,
+                      relief=FLAT,
+                      compound=CENTER)
     
     def create_label(window):
         return Label(window,
+                     bg=BACKGROUND_COLOR,
+                     fg=FONT_COLOR,
                      font=FONT_TYPE)
     
     def create_text(window):
         return Text(window,
+                    height=10,
+                    padx=20,
+                    pady=20,
+                    bg=BACKGROUND_COLOR,
+                    fg=FONT_COLOR,
                     font=FONT_TYPE,
-                    height=10)
+                    spacing3=10,
+                    wrap=WORD)
     
     def create_frame(window):
-        return Frame(window)
+        return Frame(window,
+                     bg=BACKGROUND_COLOR)
